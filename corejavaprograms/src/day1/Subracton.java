71723980da239104900358d5a32198c483270235
Abishek0070 <abishekbalamurugan858@gmail.com>
2025-08-23 14:37:49 +0530

    Subraction_java

--- corejavaprograms/src/day1/Subracton.java
+++ corejavaprograms/src/day1/Subracton.java
@@ -1,5 +1,11 @@
 package day1;
 
 public class Subracton {
-
+	public static void main (String[]args) {
+		int n = 23;
+		int m = 43;
+		int sub = 0;
+		sub = n + m;
+		System.out.println("Addition of two numbers :" + sub);
+	}
 }
